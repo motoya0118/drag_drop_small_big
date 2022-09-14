e9ddcb83f183440f1470951489d65b0705fd2d49
motoya <90805736+motoya0118@users.noreply.github.com>
2022-09-14 20:23:06 +0900

    Revert "魔改造"

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,13 +1,10 @@
 <template>
-  <div>
-    <draggable v-model="lists" :options="{group: 'lists'}" class="board dragArea" @end="listMoved">
-      <list v-for="(list, index) in lists" :list="list" :key="index"></list>
-    </draggable>
-  </div>
   <!-- <draggable v-model="bigs" :options='bigoption' @end="listMoved">
     <div v-for="big in bigs">{{big.name}}</div>
   </draggable> -->
-  
+  <draggable v-model="lists" :options="{group: 'lists'}" class="board dragArea" @end="listMoved">
+    <list v-for="(list, index) in lists" :list="list"></list>
+  </draggable>
 </template>
 
 <script>
@@ -17,47 +14,10 @@ import list from './components/list'
 export default {
   components: { draggable, list },
 
-  // props:["original_lists"],
+  props:["original_lists"],
 
-  data() {
-    return {
-      // lists: this.original_lists,
-      lists: [
-        {
-          "name": "John",
-          "id": 0,
-          "kanban_smalls": [
-            {
-              "id": 1,
-              "name": "small1",
-              "cards": [
-                {
-                  "content": "コンテンツ1",
-                  "id": 1
-                },
-                {
-                  "content": "コンテンツ2",
-                  "id": 2
-                }
-              ]
-            },
-            {
-              "id": 2,
-              "name": "small2",
-              "cards": [
-                {
-                  "content": "コンテンツ2-1",
-                  "id": 3
-                },
-                {
-                  "content": "コンテンツ2-2",
-                  "id": 4
-                }
-              ]
-            }
-          ]
-        },
-      ],
+  data() {return{
+      lists: this.original_lists,
       bigs: [{name: "大"}],
       bigoption:{
         group:{
@@ -66,9 +26,7 @@ export default {
           put: false
         },
         sort: false
-      },
-    inputText: "",
-    }
+    }}
   },
   methods: {
     listMoved: function(event) {
